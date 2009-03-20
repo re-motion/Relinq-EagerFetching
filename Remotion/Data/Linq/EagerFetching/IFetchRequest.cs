136e1afb67b6648a7cdf37c36a2701d445cb7cb6
Fabian Schmied <fabian.schmied@rubicon.eu>
2009-03-20 11:48:36 +0000

    Added IRelationEndPointDefinition.GetOppositeClassDefinition extension method (RelationEndPointDefinitionExtensions) Implemented eager fetching in QueryExecutorBase: added QueryExecutorBase.CreateQuery overloads taking fetch requests and generating IQuery instances for those Changed QueryExecutorBase.CreateQuery overload creating the query from the finished statement to take storage provider ID instead of ClassDefinition Added QueryExecutorBase.CreateQuery overload taking the ClassDefinition of the result objects Changed ParseResultCollector not to accept null projections any longer Added ParseResultCollector.AddIdentityProjectionExpression Changed OrderByExpressionParser and WhereExpressionParser to generate identity projections instead of null projections (RM-1089)

--- Remotion/Data/Linq/EagerFetching/IFetchRequest.cs
+++ Remotion/Data/Linq/EagerFetching/IFetchRequest.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Reflection;
+using Remotion.Data.Linq.Clauses;
 
 namespace Remotion.Data.Linq.EagerFetching
 {
@@ -18,5 +19,26 @@ namespace Remotion.Data.Linq.EagerFetching
     /// </summary>
     /// <value>The recursive inner fetch requests of this request.</value>
     IEnumerable<IFetchRequest> InnerFetchRequests { get; }
+
+    /// <summary>
+    /// Creates a <see cref="MemberFromClause"/> that represents the <see cref="FetchRequest{TRelated}.RelatedObjectSelector"/>. This can be inserted into a 
+    /// <see cref="QueryModel"/> in order to construct an eager-fetch query.
+    /// </summary>
+    /// <param name="selectClauseToFetchFrom">The <see cref="SelectClause"/> that is used as a template to fetch from. The new 
+    /// <see cref="MemberFromClause"/> is created in such a way that it can replace <paramref name="selectClauseToFetchFrom"/>. Its 
+    /// <see cref="AdditionalFromClause.ProjectionExpression"/> selects the fetched related objects.</param>
+    /// <param name="fromIdentifierName">The name of the <see cref="FromClauseBase.Identifier"/> to use for the new <see cref="MemberFromClause"/>.</param>
+    /// <returns>A new <see cref="MemberFromClause"/> representing the <see cref="FetchRequest{TRelated}.RelatedObjectSelector"/>.</returns>
+    /// <remarks>
+    /// <see cref="FetchRequest{TRelated}.CreateFetchQueryModel"/> uses the <see cref="MemberFromClause"/> returned by this method as follows:
+    /// <list type="number">
+    ///   <item>It clones the <see cref="QueryModel"/> representing the original query.</item>
+    ///   <item>It adds the <see cref="MemberFromClause"/> as the last body clause to the clone.</item>
+    ///   <item>It generates a new <see cref="SelectClause"/> and attaches it to the clone.</item>
+    /// </list>
+    /// </remarks>
+    MemberFromClause CreateFetchFromClause (SelectClause selectClauseToFetchFrom, string fromIdentifierName);
+
+    QueryModel CreateFetchQueryModel (QueryModel originalQueryModel);
   }
 }
